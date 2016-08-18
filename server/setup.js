f29639c975cdbd9e3ff38545a6653c210e150307
Michael Schaufelberger <michael.schufi@gmail.com>
2016-08-18 14:37:14 +0200

    setupServer method runs now as startup script

--- server/setup.js
+++ server/setup.js
@@ -1,67 +1,64 @@
-Meteor.methods( {
-	setupServer: function( argument ) {
+Meteor.startup( function () {
+	/* ### API key in external file  ### */
 
-		/* ### API key in external file  ### */
+	let realmInfo = HTTP.call( 'GET', "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/realm?api_key=" + apiKey )
+	let ddBaseURL = realmInfo.data.cdn
+	let ddBaseURLv = realmInfo.data.cdn + "/" + realmInfo.data.v
+	
+	let champImageData = HTTP.call( 'GET', "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=image,skins&api_key=" + apiKey )
 
-		let realmInfo = HTTP.call( 'GET', "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/realm?api_key=" + apiKey )
-		let ddBaseURL = realmInfo.data.cdn
-		let ddBaseURLv = realmInfo.data.cdn + "/" + realmInfo.data.v
-		
-		let champImageData = HTTP.call( 'GET', "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=image,skins&api_key=" + apiKey )
+	for ( champion in champImageData.data.data ) {
+		let championData = champImageData.data.data[champion]
+		Champions.upsert( {
+				id: championData.id
+			},
+			{ $set: {
+				id: championData.id,
+				key: championData.key,
+				name: championData.name,
+				title: championData.title
+			}
+		} )
+		ChampionImageData.upsert( {
+				championId: championData.id
+			},
+			{ $set: {
+				championId: championData.id,
+				icon: ddBaseURLv + "/img/champion/" + championData.image.full,
+				skins: championData.skins.map( skin => {
+					return {
+						id: skin.id,
+						name: skin.name,
+						loading: ddBaseURL + "/img/champion/loading/" +  championData.key + "_" + skin.num + ".jpg",
+						splash: ddBaseURL + "/img/champion/splash/" +  championData.key + "_" + skin.num + ".jpg"
+					}
+				} )
+			}
+		} )	
+	}
 
-		for ( champion in champImageData.data.data ) {
-			let championData = champImageData.data.data[champion]
-			Champions.upsert( {
-					id: championData.id
-				},
-				{ $set: {
-					id: championData.id,
-					key: championData.key,
-					name: championData.name,
-					title: championData.title
-				}
-			} )
-			ChampionImageData.upsert( {
-					championId: championData.id
-				},
-				{ $set: {
-					championId: championData.id,
-					icon: ddBaseURLv + "/img/champion/" + championData.image.full,
-					skins: championData.skins.map( skin => {
-						return {
-							id: skin.id,
-							name: skin.name,
-							loading: ddBaseURL + "/img/champion/loading/" +  championData.key + "_" + skin.num + ".jpg",
-							splash: ddBaseURL + "/img/champion/splash/" +  championData.key + "_" + skin.num + ".jpg"
-						}
-					} )
-				}
-			} )	
-		}
+	let spells = HTTP.call( 'GET', "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/summoner-spell?spellData=image,cooldown&api_key=" + apiKey )
 
-		let spells = HTTP.call( 'GET', "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/summoner-spell?spellData=image,cooldown&api_key=" + apiKey )
-
-		for ( spell in spells.data.data ) {
-			let spellData = spells.data.data[ spell ]
-			SummonerSpells.upsert( {
-					id: spellData.id
-				},
-				{ $set: {
-					id: spellData.id,
-					key: spellData.key,
-					name: spellData.name,
-					description: spellData.description,
-					cooldown: Number( spellData.cooldown[0] ) // only value in the Array
-				}
-			} )
-			SummonerSpellImageData.upsert( {
-					spellId: spellData.id
-				},
-				{ $set: {
-					spellId: spellData.id,
-					icon: ddBaseURLv + "/img/spell/" + spellData.image.full		
-				}
-			} )
-		}
+	for ( spell in spells.data.data ) {
+		let spellData = spells.data.data[ spell ]
+		SummonerSpells.upsert( {
+				id: spellData.id
+			},
+			{ $set: {
+				id: spellData.id,
+				key: spellData.key,
+				name: spellData.name,
+				description: spellData.description,
+				cooldown: Number( spellData.cooldown[0] ) // only value in the Array
+			}
+		} )
+		SummonerSpellImageData.upsert( {
+				spellId: spellData.id
+			},
+			{ $set: {
+				spellId: spellData.id,
+				icon: ddBaseURLv + "/img/spell/" + spellData.image.full		
+			}
+		} )
 	}
 } );
